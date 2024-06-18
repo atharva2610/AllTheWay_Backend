e01dd023fe5a86f10373fa814339804608001e9e
atharva2610 <atharvavishwakarma526@gmail.com>
2024-06-18 14:59:35 +0530

    updated static folder location

--- AllTheWay/settings.py
+++ AllTheWay/settings.py
@@ -132,7 +132,8 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
